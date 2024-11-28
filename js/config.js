1cd92996322578cd28685f13484b15ede2591484
Levilevs021 <131588575+Levilevs021@users.noreply.github.com>
2024-11-28 19:41:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_StLSTvUs5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3r7np3bbiuor7bg8if3nakpmab', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
