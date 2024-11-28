873c94f7f036449fff8831ce78ab4413cd7a5e40
Levilevs021 <131588575+Levilevs021@users.noreply.github.com>
2024-11-28 20:03:53 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_hiwIawa7M', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6u92qkltg9t6ucl806mhv7bkhf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_H3mmEJYrC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3d3l0m3pihokpqo8llp75ff3r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
